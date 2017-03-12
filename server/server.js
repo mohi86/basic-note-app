219b3f7b17d6ee34d79544170fae75bff7da011b
mohi86 <mohi86@live.com>
2017-03-13 12:36:32 +1300

    added Get /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const {ObjectID} = require('mongodb')
 
 const {mongoose} = require('./db/mongoose')
 const {Todo} = require('./models/todo')
@@ -26,8 +27,25 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos})
-    },(e) => {
+    }, (e) => {
         res.send(400).send(e);
+    })
+})
+
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id
+
+    if (!ObjectID.isValid(id)) {
+        console.log('Todo id not valid')
+        return res.status(400).send({error: 'Todo id not valid'})
+    }
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send({error: 'Todo not found'})
+        }
+        res.status(200).send({todo})
+    }).catch((e) => {
+        res.status(400).send({error: 'Bad request'})
     })
 })
 
