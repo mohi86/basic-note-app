7460e78feb8c5e2afcee3f5e0dced3e12ef4c891
mohi86 <mohi86@live.com>
2017-03-13 07:17:19 +1300

    Add Get /todos route and test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,19 @@ const request = require('supertest')
 const {app} = require('./../server')
 const {Todo} = require('./../models/todo')
 
+//Seed data
+const todos = [{
+    text: 'firt items on todos'
+},{
+    text: 'Second items on todos'
+},{
+    text: 'third items on todos'
+}]
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done())
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos)
+    }).then(() => done())
 })
 
 describe('POST /todos', () => {
@@ -23,7 +34,7 @@ describe('POST /todos', () => {
                 if (err) {
                     return done(err)
                 }
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1)
                     expect(todos[0].text).toBe(text)
                     done()
@@ -43,9 +54,21 @@ describe('POST /todos', () => {
                     return done(err)
                 }
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0)
+                    expect(todos.length).toBe(3)
                     done()
                 }).catch((e) => {done(e)})
             })
+    })
+})
+
+describe('Get /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(3)
+            })
+            .end(done)
     })
 })
