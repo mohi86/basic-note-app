21d31b9c6e8cb519a8af8f34209a3c23ea20fa32
mohi86 <mohi86@live.com>
2017-03-13 16:12:37 +1300

    test Patch /todo

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -126,3 +126,43 @@ describe('DELETE /todos/:id ', () => {
             })
     })
 })
+
+describe('PATCH /todos/:id', () => {
+    it('should update the todo', (done) => {
+        hexId = todos[1]._id.toHexString()
+        let text = "Some changed text"
+
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                completed: true,
+                text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text)
+                expect(res.body.todo.completed).toBe(true)
+                expect(res.body.todo.completedAt).toBeA('number')
+            })
+            .end(done)
+    })
+
+    it('should clear completedAt when todo is not completed', (done) => {
+        hexId = todos[0]._id.toHexString()
+        let text = "Some changed text dah"
+
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                completed: false,
+                text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text)
+                expect(res.body.todo.completed).toBe(false)
+                expect(res.body.todo.completedAt).toNotExist()
+            })
+            .end(done)
+    })
+})
