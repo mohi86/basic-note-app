636fa0edd99281e2db3cd6a505ac4b8d71da5f50
mohi86 <mohi86@live.com>
2017-03-14 06:29:12 +1300

    Mongoose schemas added and JWT created

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,57 @@
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
+const validator = require('validator')
+const jwt = require('jsonwebtoken')
+const _ = require("lodash");
 
-const User = mongoose.model('User', {
+const UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
         minlength: 2,
-        trim: true
+        trim: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
     },
+    password: {
+        type: String,
+        require: true,
+        minlength: 4
+    },
+    tokens: [{
+        access: {
+            type:String,
+            required:true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
+
+UserSchema.methods.toJSON = function () {
+    let user = this
+    let userObject = user.toObject()
+
+    return _.pick(userObject, ['_id', 'email'])
+}
+
+UserSchema.methods.generateAuthToken = function () {
+    let user = this
+    const access = 'auth'
+    const token = jwt.sign({_id: user._id.toHexString(), access }, 'abc123').toString()
+
+    user.tokens.push({
+        access, token
+    })
+    return user.save().then(() => {
+        return token
+    })
+}
+
+const User = mongoose.model('User', UserSchema)
 
 module.exports = {User}
