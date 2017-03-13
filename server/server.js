e905098cfc25678fe2470f4a8c777580e809b06d
mohi86 <mohi86@live.com>
2017-03-13 13:07:52 +1300

    preparing to deploy to heroku

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const {Todo} = require('./models/todo')
 const {User} = require('./models/user')
 
 var app = express()
+const PORT = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 
@@ -49,8 +50,8 @@ app.get('/todos/:id', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Started on port 3000')
+app.listen(PORT, () => {
+    console.log(`Started at port ${PORT}`)
 })
 
 module.exports = {app}
