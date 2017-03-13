82528a8ed6ef2eef507c4edfacf8fb8720481dd3
mohi86 <mohi86@live.com>
2017-03-13 14:57:14 +1300

    DELETE route added to /todos/:id

--- server/server.js
+++ server/server.js
@@ -50,6 +50,23 @@ app.get('/todos/:id', (req, res) => {
     })
 })
 
+app.delete('/todos/:id', (req, res) => {
+    let id = req.params.id
+
+    if (!ObjectID.isValid(id)) {
+        console.log('ID not valid!')
+        return res.status(400).send({error: 'Todo id not valid'});
+    }
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send({error: 'Todo not found'})
+        }
+        res.status(200).send({todo})
+    }).catch((e) => {
+        res.status(400).send({error: 'Bad request'})
+    })
+})
+
 app.listen(PORT, () => {
     console.log(`Started at port ${PORT}`)
 })
