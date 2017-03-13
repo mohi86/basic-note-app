fa3c2448310fed5c9a93a878bd4dda5466b62872
mohi86 <mohi86@live.com>
2017-03-14 07:21:15 +1300

    GET /users/me private route with middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb')
 const {mongoose} = require('./db/mongoose')
 const {Todo} = require('./models/todo')
 const {User} = require('./models/user')
+const {authenticate} = require('./middleware/authenticate')
 
 var app = express()
 const PORT = process.env.PORT || 3000
@@ -107,6 +108,10 @@ app.post('/users', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e)
     })
+})
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user)
 })
 
 app.listen(PORT, () => {
